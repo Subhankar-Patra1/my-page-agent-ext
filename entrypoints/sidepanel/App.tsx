7fc554031000aee90d6535364c223f8b8fbc4078
AlgorithSage <carchisman1@gmail.com>
2026-05-17 01:31:48 +0530

    draft 17

--- entrypoints/sidepanel/App.tsx
+++ entrypoints/sidepanel/App.tsx
@@ -7,7 +7,7 @@ import remarkGfm from 'remark-gfm';
 import { ChartBlock } from './components/ChartBlock';
 import { storage } from '@wxt-dev/storage';
 import {
-  RocketLaunchIcon, EyeIcon, LightningIcon, ClockCounterClockwiseIcon,
+  ClockCounterClockwiseIcon,
   CheckCircleIcon, XCircleIcon, WarningIcon, InfoIcon,
 } from '@phosphor-icons/react';
 import './App.css';
@@ -198,21 +198,21 @@ export default function App() {
             
             <div className="landing-features">
               <div className="feature-item">
-                <span className="feature-icon"><RocketLaunchIcon size={20} color="#f97316" weight="duotone" /></span>
+                <span className="feature-icon"><img src="/icon/artificial.png" width={20} height={20} alt="Autonomous Execution" /></span>
                 <div className="feature-text">
                   <strong>Autonomous Execution</strong>
                   <span>Give me a goal and watch me navigate.</span>
                 </div>
               </div>
               <div className="feature-item">
-                <span className="feature-icon"><EyeIcon size={20} color="#f97316" weight="duotone" /></span>
+                <span className="feature-icon"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24"><path fill="#f97316" d="M16 18a1 1 0 100-2v2zm-8-2a1 1 0 100 2v-2zm.707-8.707a1 1 0 10-1.414 1.414l1.414-1.414zM11 11l.707.707a1 1 0 000-1.414L11 11zm-3.707 2.293a1 1 0 101.414 1.414l-1.414-1.414zM7 4h10V2H7v2zm13 3v10h2V7h-2zm-3 13H7v2h10v-2zM4 17V7H2v10h2zm3 3a3 3 0 01-3-3H2a5 5 0 005 5v-2zm13-3a3 3 0 01-3 3v2a5 5 0 005-5h-2zM17 4a3 3 0 013 3h2a5 5 0 00-5-5v2zM7 2a5 5 0 00-5 5h2a3 3 0 013-3V2zm9 14H8v2h8v-2zM7.293 8.707l3 3 1.414-1.414-3-3-1.414 1.414zm3 1.586l-3 3 1.414 1.414 3-3-1.414-1.414z"/></svg></span>
                 <div className="feature-text">
                   <strong>Visual DOM Perception</strong>
                   <span>Advanced spatial understanding of web layouts.</span>
                 </div>
               </div>
               <div className="feature-item">
-                <span className="feature-icon"><LightningIcon size={20} color="#f97316" weight="duotone" /></span>
+                <span className="feature-icon"><img src="/icon/chat.svg" width={20} height={20} alt="Intelligent Summarization" /></span>
                 <div className="feature-text">
                   <strong>Intelligent Summarization</strong>
                   <span>Concise insights and actionable results.</span>
